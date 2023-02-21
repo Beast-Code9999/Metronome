231c197f81431cfaed7e98049eb7b983ce44622f
Beast Code <beastcode9999@gmail.com>
2023-02-21 11:11:53 +0900

    add footer script

---
+++ script.js
@@ -0,0 +1,7 @@
+// footer script
+function addDynamicFooterDate() {
+    const footer = document.querySelector('.footer__year');
+    let year = new Date().getFullYear();
+    footer.textContent = year;
+};
+addDynamicFooterDate();
