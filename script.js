6437196d651c1b2ca91eea389acb82ad5176855c
Beast Code <beastcode9999@gmail.com>
2023-02-24 08:43:02 +0900

    add Metronome()

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const Metronome = (function Metronome {
+
+    const init = function init() {
+
+    }
+
+    return {
+        init: init,
+    }
+})();
+
+
 // footer script
 function addDynamicFooterDate() {
     const footer = document.querySelector('.footer__year');
